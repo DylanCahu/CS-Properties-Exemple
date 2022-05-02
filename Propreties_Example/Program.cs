17fb847aef96a5e894538c378612ae5049d84c37
DylanCahu <dydypot@gmail.com>
2022-05-02 09:08:56 +0200

    first

---
+++ Propreties_Example/Program.cs
@@ -0,0 +1,45 @@
+﻿
+class ClassDate
+{
+    //private int _jour, _mois, _annee;
+    public int jour { get; set; }
+    public int mois { get; set; }
+    public int annee { get; set; }
+    public string date{ get
+        {
+            return jour + "/" + mois + "/" + annee;
+        }
+        set
+        {
+            string[] chn = value.Split('/');
+            jour = Convert.ToInt32(chn[0]);
+            mois = Convert.ToInt32(chn[1]);
+            annee = Convert.ToInt32(chn[2]);
+        } 
+    }
+}
+
+
+class Program
+{
+
+
+    static void Main(string[] args) 
+    {
+        Console.WriteLine("Propriétés");
+        Console.WriteLine("==========");
+
+        /*ExempleGetSet ex1 = new ExempleGetSet(123, "coucou")
+        ex1.print();
+        Console.ReadLine();*/
+
+        ClassDate date1 = new ClassDate();
+
+        date1.date = "01/02/2021";
+        Console.WriteLine(date1.date + " Day : " + date1.jour + " Month : " +date1.mois + " Year : " + date1.annee);
+
+
+    }
+
+    
+}
